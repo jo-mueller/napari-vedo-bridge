c2528caa4da2465720c83d61c52f0b5c13e53619
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-09 09:28:46 +0200

    added support for 4d surface writing

--- src/napari_vedo_bridge/_writer.py
+++ src/napari_vedo_bridge/_writer.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 from napari_timelapse_processor import TimelapseConverter
-from napari.layers import Points, Layer
+from napari.layers import Points, Layer, Surface
 
 def write_points(
         path: str,
@@ -46,3 +46,49 @@ def write_points(
 
         return output_paths
     
+
+def write_surfaces(
+        path: str,
+        layer_data: Any,
+        attributes: dict
+) -> List[str]:
+    import vedo
+    import os
+    from pathlib import Path
+    import tqdm
+
+    layer = Layer.create(layer_data, attributes, 'surface')
+    # if 'features' in  attributes:
+    #     layer.features = attributes['features']
+    #     attributes.pop('features')
+
+    # is it 4D?
+    if layer.data[0].shape[1] == 4:
+        Converter = TimelapseConverter()
+        list_of_layers = Converter.unstack_data(layer, layertype=Surface)
+    else:
+        list_of_layers = [layer]
+
+    # if there is only one timepoint, just write it
+    if len(list_of_layers) == 1:
+        mesh = vedo.Mesh(list_of_layers[0].data)
+        for key in dict(list_of_layers[0].features).keys():
+            mesh.pointdata[key] = list_of_layers[0].features[key]
+        vedo.write(mesh, path)
+        return [path]
+    
+    # if there are multiple timepoints, write each one separately
+    else:
+        os.makedirs(Path(path).parent / Path(path).stem, exist_ok=True)
+        output_paths = []
+        for i, layer in tqdm.tqdm(enumerate(list_of_layers), total=len(list_of_layers)):
+            mesh = vedo.Mesh((layer.data[0], layer.data[1].astype(int)))
+            for key in dict(list_of_layers[0].features).keys():
+                mesh.pointdata[key] = list_of_layers[i].features[key]
+
+            output_path = str(Path(path).parent / Path(path).stem / "{:03d}.vtp".format(i))
+            output_paths.append(output_path)
+            
+            vedo.write(mesh, output_path)
+
+        return output_paths

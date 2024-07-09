7835896f8ec35de3166a16ae53b3089f00aa8e50
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-07-09 09:28:33 +0200

    addedd support for 4d surface reading

--- src/napari_vedo_bridge/_reader.py
+++ src/napari_vedo_bridge/_reader.py
@@ -1,26 +1,41 @@
 from typing import Union, Sequence, Callable, List, Optional
 PathLike = str
 PathOrPaths = Union[PathLike, Sequence[PathLike]]
+from napari.layers import Layer
 #ReaderFunction = Callable[[PathOrPaths], List[LayerData]]
 
 def get_reader(path: "PathOrPaths") -> Optional["ReaderFunction"]:
     import os
+    import vedo
+
+    # in case a single file is passed
     if isinstance(path, str) and path.endswith('.vtp'):
-        return points_reader
+        thing = vedo.load(path)
+        if isinstance(thing, vedo.Points):
+            return points_reader
+        elif isinstance(thing, vedo.Mesh):
+            return surfaces_reader
+
+    # in case a list of files is passed
     elif isinstance(path, list) and all([p.endswith('.vtp') for p in path]):
-        return points_reader
+        thing = vedo.load(path[0])
+        if isinstance(thing, vedo.Points):
+            return points_reader
+        elif isinstance(thing, vedo.Mesh):
+            return surfaces_reader
+
+    # in case a directory is passed
+    # find all files inside and pass again as list
     elif isinstance(path, str) and os.path.isdir(path):
-        return points_reader
+        filenames = [os.path.join(path, f) for f in os.listdir(path)] 
+        return get_reader(filenames)
 
     return None
 
 def points_reader(path: PathOrPaths) -> List["LayerData"]:
-    import vedo
-    import pandas as pd
     import os
     import tqdm
     from napari_timelapse_processor import TimelapseConverter
-    from napari.layers import Layer
 
     # whether directory, list of files or single file is passed
     if os.path.isdir(path):
@@ -32,13 +47,7 @@ def points_reader(path: PathOrPaths) -> List["LayerData"]:
 
     layers = []
     if len(path) == 1:
-        points = vedo.load(path[0])
-        layer = (
-            points.vertices,
-            {'features': pd.DataFrame(dict(points.pointdata))},
-            'points'
-        )
-
+        layer = _read_single_points(path[0])
         data = layer.data
         properties = {
             'features': layer.features,
@@ -48,14 +57,7 @@ def points_reader(path: PathOrPaths) -> List["LayerData"]:
     else:
         # read all the files, create a layer from each
         for i, p in tqdm.tqdm(enumerate(path), total=len(path)):
-            points = vedo.load(p)
-            layer = Layer.create(
-                points.vertices,
-                {
-                    'features': pd.DataFrame(dict(points.pointdata)),
-                    'size': 0.5,
-                    },
-                'points')
+            layer = _read_single_points(p)
             layers.append(layer)
 
         # Stack output layer and return
@@ -67,8 +69,85 @@ def points_reader(path: PathOrPaths) -> List["LayerData"]:
             'size': 0.5
         }
 
-    # color the pointclloud by the first detected feature
+    # color the pointcloud by the first detected feature
     if not properties['features'].empty:
         properties['face_color'] = properties['features'].columns[0]
     
     return [(data, properties, 'points')]
+
+def surfaces_reader(path: PathOrPaths) -> List["LayerData"]:
+    import os
+    import tqdm
+    from napari_timelapse_processor import TimelapseConverter
+
+    # whether directory, list of files or single file is passed
+    if os.path.isdir(path):
+        path = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.vtp')]
+    elif isinstance(path, list):
+        pass
+    elif isinstance(path, str):
+        path = [path]
+
+    layers = []
+    if len(path) == 1:
+        layer = _read_single_surface(path[0])
+        data = layer.data
+        properties = {
+            'features': layer.features,
+            'size': 0.5
+        }
+
+    else:
+        # read all the files, create a layer from each
+        for i, p in tqdm.tqdm(enumerate(path), total=len(path)):
+            layer = _read_single_surface(p)
+            layers.append(layer)
+
+        # Stack output layer and return
+        Converter = TimelapseConverter()
+        layer_4d = Converter.stack_data(layers, layertype=Layer)
+        data = layer_4d.data
+        properties = {
+            'features': layer_4d.features,
+            'size': 0.5
+        }
+
+    # color the pointcloud by the first detected feature
+    if not properties['features'].empty:
+        properties['face_color'] = properties['features'].columns[0]
+    
+    return [(data, properties, 'surface')]
+
+
+def _read_single_points(path) -> Layer:
+    """
+    Read a single points file and return a Layer object
+    """
+    import vedo
+    import pandas as pd
+    points = vedo.load(path)
+
+    layer = (
+            points.vertices,
+            {'features': pd.DataFrame(dict(points.pointdata))},
+            'points'
+        )
+    
+    return layer
+
+def _read_single_surface(path):
+    """
+    Read a single surface file and return a Layer object
+    """
+    import vedo
+    import pandas as pd
+    surface = vedo.load(path)
+
+    layer = (
+            surface.points(),
+            {},
+            'surface'
+        )
+    layer.features = pd.DataFrame(dict(surface.pointdata))
+    
+    return layer

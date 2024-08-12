89a5c572db99efcb907e7452c4c8b9ab93e9a3e7
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-08-12 13:10:55 +0200

    made outlier removal test more meaningful

--- src/napari_vedo_bridge/_tests/test_points.py
+++ src/napari_vedo_bridge/_tests/test_points.py
@@ -3,14 +3,20 @@ import numpy as np
 from napari_vedo_bridge._points import smooth_mls_1d, remove_outliers
 from napari.layers import Points
 
+
 @pytest.fixture
 def sample_points():
     return Points(np.random.randn(1000, 3))
+
 
 def test_smooth_points(sample_points):
     smoothed_points = smooth_mls_1d()(sample_points, factor=0.2, radius=0)
     assert smoothed_points.data.shape == sample_points.data.shape
 
+
 def test_remove_outliers(sample_points):
-    filtered_points = remove_outliers()(sample_points, radius=0.1, n_neighbors=5)
-    assert filtered_points.data.shape[1] == 3
+    # add a far-out point
+    sample_points.data[0] = [100, 100, 100]
+
+    filtered_points = remove_outliers()(sample_points, radius=2.5, n_neighbors=1)
+    assert filtered_points.data.shape[0] == sample_points.data.shape[0] - 1
